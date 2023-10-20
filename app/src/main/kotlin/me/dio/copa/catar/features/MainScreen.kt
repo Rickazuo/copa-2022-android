79a1aea695dee82ab4b267e9f17502798279bcce
Rickazuo <rick3209@gmail.com>
2023-10-20 20:20:35 -0300

    finshed project

--- app/src/main/kotlin/me/dio/copa/catar/features/MainScreen.kt
+++ app/src/main/kotlin/me/dio/copa/catar/features/MainScreen.kt
@@ -1,9 +1,7 @@
 package me.dio.copa.catar.features
 
-import android.app.Notification
-import android.app.appsearch.SearchResult.MatchInfo
-import android.icu.text.CaseMap.Title
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -11,7 +9,6 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
@@ -22,7 +19,6 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.scale
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
@@ -33,9 +29,10 @@ import me.dio.copa.catar.domain.model.MatchDomain
 import me.dio.copa.catar.domain.model.TeamDomain
 import me.dio.copa.catar.ui.theme.Shapes
 
+typealias NotificationOnClick = ((match: MatchDomain) -> Unit)
 
 @Composable
-fun MainScreen(matches: List<MatchDomain>) {
+fun MainScreen(matches: List<MatchDomain>, onNotificationClick: NotificationOnClick) {
 
 
 
@@ -44,14 +41,14 @@ fun MainScreen(matches: List<MatchDomain>) {
         .padding(8.dp)){
         LazyColumn(){
             items(matches) { match ->
-                MatchInfo(match)
+                MatchInfo(match, onNotificationClick)
             }
         }
     }
 }
 
 @Composable
-fun MatchInfo(match: MatchDomain) {
+fun MatchInfo(match: MatchDomain, onNotificationClick: NotificationOnClick) {
     Card(
         shape = Shapes.large,
         modifier = Modifier.fillMaxWidth()
@@ -63,7 +60,7 @@ fun MatchInfo(match: MatchDomain) {
 
 //modifier = Modifier.padding(8.dp)
             Column(modifier = Modifier.padding(8.dp)) {
-                Notification(match)
+                Notification(match, onNotificationClick)
                 Title(match)
                 Teams(match)
             }
@@ -72,11 +69,16 @@ fun MatchInfo(match: MatchDomain) {
 }
 
 @Composable
-fun Notification(match: MatchDomain) {
+fun Notification(match: MatchDomain, onClick: NotificationOnClick) {
     Row(modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.End) {
         val drawable = if (match.notificationEnabled) R.drawable.ic_notifications_active
         else R.drawable.ic_notifications
-        Image(painter = painterResource(id = drawable), contentDescription = null)
+        Image(
+            painter = painterResource(id = drawable),
+            modifier = Modifier.clickable {
+                 onClick(match)
+            },
+            contentDescription = null)
     }
 }
 
